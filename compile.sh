73584c9ae5c81275a5bfb5a8a96281e6a637d8aa
Travis Ryan <travis.ryan@iinet.net.au>
2025-08-19 12:35:40 +0800

    modified compile script

--- compile.sh
+++ compile.sh
@@ -3,6 +3,13 @@
 # Exit if any command fails
 set -e
 
+set -euo pipefail
+_safe_source() { if set -o | grep -q 'nounset *on'; then u=1; set +u; else u=0; fi; source "$1"; [ "$u" = 1 ] && set -u; }
+[ -z "${ROS_DISTRO:-}" ] && { . /etc/os-release; [ "$VERSION_CODENAME" = "focal" ] && ROS_DISTRO=foxy || ROS_DISTRO=humble; }
+[ -f "/opt/ros/$ROS_DISTRO/setup.bash" ] && _safe_source "/opt/ros/$ROS_DISTRO/setup.bash"
+[ -f "$HOME/humanoid_ws/install/setup.bash" ] && _safe_source "$HOME/humanoid_ws/install/setup.bash"
+
+
 # Define paths
 LIVOX_DIR="$HOME/humanoid_ws/src/livox_ros_driver2/"
 HUMANOID_WS="$HOME/humanoid_ws/"
